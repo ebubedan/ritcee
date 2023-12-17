55a6c9e91b5ca4fd09ed9f2f7fdbfeca2135945a
ebubedan <ebubedanielude@gmail.com>
2023-12-16 13:56:21 -1200

    pictures updating changes

--- auntyrita.js
+++ auntyrita.js
@@ -13,3 +13,33 @@ document.onload = () => {
         document.querySelector('.header').classList.remove('active');
     }
 };
+
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
+
+// Add this script at the end of your body or in a separate script file
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        // Add breakpoints for responsive design
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 30,
+        },
+    },
+});
+
